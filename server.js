009b3ca8bd5cf11f73da45bbf358c4c22f669ff6
bea andrea <petermagalong0421@gmail.com>
2022-03-11 16:08:58 -0800

    Complete Add to Cart Funtionality BE

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/api/products', (req,res) => {
 res.send(data.products)
 });
 app.get('/api/products/slug/:slug', (req,res) => {
-    const product = data.products.find(x => x.slug === req.params.slug);
+    const product = data.products.find((x) => x.slug === req.params.slug);
     if(product) {
         res.send(product);
     }
@@ -15,6 +15,14 @@ app.get('/api/products/slug/:slug', (req,res) => {
         res.status(404).send({message: 'Product not Found'})
     }
     });
+    app.get('/api/products/:id', (req, res) => {
+        const product = data.products.find((x) => x._id === req.params.id);
+        if (product) {
+          res.send(product);
+        } else {
+          res.status(404).send({ message: 'Product Not Found' });
+        }
+      });
 
 const port = process.env.PORT || 5000 //free access sa mga free ports
 app.listen(port, () =>{
